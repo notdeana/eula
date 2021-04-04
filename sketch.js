0ac6e3f02a885aaf9b4a23bee581708276f0049e
おれわころせ <32080425+notdeana@users.noreply.github.com>
2021-04-03 22:39:04 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,29 +11,51 @@ let eula = [{
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
   createCanvas(400, 400);
+  background(300);
+  textSize(32);
+  
+  text("SEIZURE WARNING", 50, 50);
+  setInterval(changeBackground, 3000);
 
-randomIndex = int(random(dogs.length));
-
-console.log(eula[randomIndex].name);
-text(eula[randomIndex].name, 50, 50);
-
-eula.splice(randomIndex, 1);
-console.log(eula);
 
 }
 
 
 function draw() {
+
+  if (animating == true){
+    ellipse(random(width), random(height), random(70,300));
+  }
+}
+
+function changeBackground(){
+  background(random(255), random(255), random(255));
+    setInterval(changeBackground, 3000);
+}
+function randomizer(){
+  animating = false;
+   if (eula[0]){
+      background(200,650,400);
+ randomIndex = int(random(eula.length));
+text(`${eula[randomIndex].name}'s favorite color is
+
+${eula[randomIndex].color}`, 50, 50);
+
+eula.splice(randomIndex, 1);
+console.log(eula);
+
+} else {
   background(200,650,400);
+  text("game over!", 50, 50);
+}
 }
 
 function mousePressed() {
-  background(random(200, 650,400));
-  randomIndex = int(random(dogs.length));
-  text(eula[randomIndex].name, 50, 50);
-  eula.splice(randomIndex, 1);
-
+  animating = true;
+  setTimeout(randomizer, 2000);
+ 
 }
